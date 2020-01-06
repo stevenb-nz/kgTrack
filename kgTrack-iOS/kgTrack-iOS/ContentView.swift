52d8503a77325bce2eb796ee81ebc55c4eae2039
Steven Brown <sgbrown@mac.com>
2020-01-06 22:47:50 +1300

    Initial commit

---
+++ kgTrack-iOS/kgTrack-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kgTrack-iOS
+//
+//  Created by Steven Brown on 6/01/20.
+//  Copyright © 2020 Steven Brown. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kgTrack-macOS/kgTrack-macOS/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  kgTrack-macOS
+//
+//  Created by Steven Brown on 6/01/20.
+//  Copyright © 2020 Steven Brown. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
